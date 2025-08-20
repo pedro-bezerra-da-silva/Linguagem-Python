bbefe46596644f6fcde87152cab50495bbf99b7f
PEDRO BEZERRA DA SILVA <pedrob.23@hotmail.com>
2025-08-20 11:19:38 -0300

    Init: Iniciando o estudo Python

---
+++ par_impar.py
@@ -0,0 +1,6 @@
+# Declara a variável que irá gaurdar um número
+valor = 13
+if(valor %2 == 0):
+    print("Esta valor é Par")
+else:
+    print("Este valor é Impar")
---
+++ pontoevirgula.py
@@ -0,0 +1,3 @@
+# Declaração de variáveis com conteúdoe separação utilizando ponto e virgula
+pais= "Brasil"; uf= "SP"; cidade = "São Paulo"; espaço = " - "
+print(pais+espaço+uf+espaço+cidade)
